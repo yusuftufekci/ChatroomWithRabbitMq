6cf349ac84a9bffd0f893fffcad234b72666278c
yusuftufekci <yusuftufekci96@gmail.com>
2023-05-20 19:04:55 +0300

    Created a signalR hub

---
+++ ChatroomWithRabbitMq/Core/Hubs/ChatroomHub.cs
@@ -0,0 +1,13 @@
+﻿using ChatroomWithRabbitMq.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatroomWithRabbitMq.Core.Hubs
+{
+    public class ChatroomHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.SendAsync("RecieveMessages", message);
+        }
+    }
+}
