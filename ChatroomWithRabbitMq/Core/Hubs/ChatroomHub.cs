b97342be777fa210793c3b65ab22dbf5a32d1558
yusuftufekci <yusuftufekci96@gmail.com>
2023-05-23 19:45:52 +0300

    Added StockBot in ChatHub

--- ChatroomWithRabbitMq/Core/Hubs/ChatroomHub.cs
+++ ChatroomWithRabbitMq/Core/Hubs/ChatroomHub.cs
@@ -1,13 +1,39 @@
-﻿using ChatroomWithRabbitMq.Models;
+﻿using ChatroomWithRabbitMq.Core.StockBot;
+using ChatroomWithRabbitMq.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatroomWithRabbitMq.Core.Hubs
 {
     public class ChatroomHub : Hub
     {
+        private readonly IStockBotService _stockBotService;
+        public ChatroomHub(IStockBotService stockBotService)
+        {
+            _stockBotService= stockBotService;
+        }
         public async Task SendMessage(Message message)
         {
-            await Clients.All.SendAsync("receiveMessage", message);
+            if (message.Text.Contains("/stock="))
+            {
+                var result = _stockBotService.GetStock(message.Text);
+                if (result == false)
+                {
+                    await Clients.All.SendAsync("receiveMessage", message);
+                }
+            }
+            else
+            {
+                await Clients.All.SendAsync("receiveMessage", message);
+            }
+            
+
+        }
+        public async Task SendStockMessage(string message)
+        {
+            
+
+                await Clients.All.SendAsync("receiveMessage", message);
+            
         }
     }
 }
