f366713ec56e5f4e992472d8f7348ef3ea4ffa4f
yusuftufekci <yusuftufekci96@gmail.com>
2023-05-23 19:49:06 +0300

    Added rabbitmq services

---
+++ ChatroomWithRabbitMq/Service/RabbitMq/Consumer.cs
@@ -0,0 +1,37 @@
+﻿using ChatroomWithRabbitMq.Core.Hubs;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ChatroomWithRabbitMq.Service.RabbitMq
+{
+    public class Consumer
+    {
+        private readonly RabbitMqService _rabbitMqService;
+        private ChatroomHub _chatroomHub;
+
+        public Consumer(string queueName)
+        {
+            _rabbitMqService = new RabbitMqService();
+
+
+            using (var connection = _rabbitMqService.GetRabbitMqConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    // Received event'i sürekli listen modunda olacaktır.
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+
+                        var message = Encoding.UTF8.GetString(body);
+                        _chatroomHub.SendStockMessage(message);
+                    };
+
+                    channel.BasicConsume(queueName, true, consumer);
+                }
+            }
+        }
+    }
+}
---
+++ ChatroomWithRabbitMq/Service/RabbitMq/RabbitMqService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Connections;
+using RabbitMQ.Client;
+
+namespace ChatroomWithRabbitMq.Service.RabbitMq
+{
+    public class RabbitMqService
+    {
+        private readonly string _hostName = "localhost";
+
+        public IConnection GetRabbitMqConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = _hostName
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
