0bd3058e09d3cc97ee644e9d2bc8db675c32107b
yusuftufekci <yusuftufekci96@gmail.com>
2023-05-23 20:19:21 +0300

    Added a consumer- But doesnt work

--- ChatroomWithRabbitMq/Service/RabbitMq/Consumer.cs
+++ ChatroomWithRabbitMq/Service/RabbitMq/Consumer.cs
@@ -1,14 +1,17 @@
 ﻿using ChatroomWithRabbitMq.Core.Hubs;
+using ChatroomWithRabbitMq.Core.StockBot;
+using Microsoft.AspNetCore.SignalR;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace ChatroomWithRabbitMq.Service.RabbitMq
 {
-    public class Consumer
+    public class Consumer : Hub
     {
         private readonly RabbitMqService _rabbitMqService;
-        private ChatroomHub _chatroomHub;
+        private readonly IStockBotService _stockBotService;
+
 
         public Consumer(string queueName)
         {
@@ -26,9 +29,14 @@ namespace ChatroomWithRabbitMq.Service.RabbitMq
                         var body = ea.Body.ToArray();
 
                         var message = Encoding.UTF8.GetString(body);
-                        _chatroomHub.SendStockMessage(message);
+                        if(message != null)
+                        {
+                            Clients.All.SendAsync("receiveMessage", message);
+                            Console.WriteLine(message);
+                        }
+                        //_chatroomHub.SendStockMessage(message);
                     };
-
+                    
                     channel.BasicConsume(queueName, true, consumer);
                 }
             }
