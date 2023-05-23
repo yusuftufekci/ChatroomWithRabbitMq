eba086138dade19190bdb00fe2583f711ac2cfec
yusuftufekci <yusuftufekci96@gmail.com>
2023-05-23 16:09:14 +0300

    Created a service layer

---
+++ ChatroomWithRabbitMq/Core/Service/IChatroomService.cs
@@ -0,0 +1,13 @@
+﻿using ChatroomWithRabbitMq.Models;
+using System.Security.Claims;
+
+namespace ChatroomWithRabbitMq.Core.Service
+{
+    public interface IChatroomService
+    {
+        Task<List<Message>> GetMessages();
+        Task CreateMessages(ClaimsPrincipal User, Message message);
+
+
+    }
+}
---
+++ ChatroomWithRabbitMq/Service/Chatroom/ChatroomService.cs
@@ -0,0 +1,56 @@
+﻿using ChatroomWithRabbitMq.Core.Service;
+using ChatroomWithRabbitMq.Data;
+using ChatroomWithRabbitMq.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace ChatroomWithRabbitMq.Service.Chatroom
+{
+    public class ChatroomService : IChatroomService
+    {
+        private readonly ApplicationDbContext _context;
+        public readonly UserManager<ChatUser> _userManager;
+
+        public ChatroomService(ApplicationDbContext context, UserManager<ChatUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task CreateMessages(ClaimsPrincipal User, Message message)
+        {
+            try
+            {
+                if(message.Text!= null)
+                {
+                    message.UserName = User.Identity.Name;
+                    var sender = await _userManager.GetUserAsync(User);
+                    message.UserId = sender.Id;
+                    await _context.Messages.AddAsync(message);
+                    await _context.SaveChangesAsync();
+                }
+                
+            }
+            catch (Exception ex) 
+            {
+                throw new InvalidOperationException("Something is wrong",ex);
+            }
+
+        }
+
+        public async Task<List<Message>> GetMessages()
+        {
+            try
+            {
+                var messages = await _context.Messages.OrderByDescending(p => p.CreateDate).Take(50).ToListAsync();
+                return messages;
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Something is wrong", ex);
+            }
+        }
+    }
+}
